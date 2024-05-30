15b6b73970d39e21a6e69f90986672c0ca163b32
Grace Setiaputri <121508296+rhastame@users.noreply.github.com>
2024-05-30 08:43:29 +0700

    checkout fix and modal

--- resources/views/customer/cart.blade.php
+++ resources/views/customer/cart.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Keranjang Saya</title>
+    <title>Keranjang</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     @vite('resources/css/customer/cart.css')
     @vite('resources/css/app.css')
 </head>
@@ -12,7 +14,7 @@
     <div class="atas">
         <div class="nonactive active">
             <h1>Keranjang</h1>
-        </div>
+        </div> 
         <div class="nonactive">
             <img class="back_icon" src="{{asset('assets/back.svg')}}">
             <h1>Checkout</h1>
@@ -64,4 +66,7 @@
 
     @include('components.customer.footercustomer')
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 </html>
