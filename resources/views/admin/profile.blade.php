ccb0e7951e3aecc8d6e5b1b98cda66c02ae45733
BintangAryo26 <bintangaryo08@gmail.com>
2024-06-09 15:02:45 +0700

    profil admin and shipping

---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Melati</title>
+    @vite('resources/css/admin/profile.css')
+    @vite('resources/css/app/css')
+</head>
+
+<body>
+    @include('components.admin.headeradmin')
+    <div class="withsidebar">
+        @include('components.admin.sidebaradmin')
+        <div class="pf-main-content ">
+            <div class="pf-inner-container">
+                <div class="pf-profile">
+                    <div class="pf-profile-info">
+                        <h3 class="pf-title">Profil</h3>
+                        <form action="" class="pf-profileform " method = "">
+                            <div class="pf-formdetail">
+                                <div class="pf-name">Nama</div>
+                                <input class="pf-textfield" type="text" id="nama" name="nama"
+                                    placeholder="Nama sebelumnya" required>
+                            </div>
+                            <div class="pf-formdetail">
+                                <div class="pf-name">Tanggal Lahir</div>
+                                <input class="pf-textfield" type="date" id="tanggal_lahir" name="tanggal_lahir"
+                                    placeholder="12/12/1222" required>
+                            </div>
+                            <div class="pf-formdetail">
+                                <div class="pf-name">Jenis Kelamin</div>
+                                <div class="pf-gender">
+                                    <div class="pf-radio"><input type="radio" id="pria" name="gender"
+                                            value="pria" required>
+                                        <label for="pria">Pria</label>
+                                    </div>
+                                    <div class="pf-radio"><input type="radio" id="wanita" name="gender"
+                                            value="wanita" required>
+                                        <label for="wanita">Wanita</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pf-formdetail">
+                                <div class="pf-name">Nomor Telepon</div>
+                                <input class="pf-textfield" type="text" id="nama" name="nama"
+                                    placeholder="Nomor sebelumnya" required>
+                            </div>
+                            <div class="pf-formdetail">
+                                <div class="pf-name">Email</div>
+                                <input class="pf-textfield" type="text" id="nama" name="nama"
+                                    placeholder="Email sebelumnya" required>
+                            </div>
+                            <div class="pf-submit-btn-container"><button class="pf-submit-btn">Simpan</button></div>
+                        </form>
+                    </div>
+                    <div class="pf-profile-pp">
+                        <!-- Profile Photo File Input -->
+                        <input name = "profile_photo_path" type="file" id="photo" class="hidden"
+                            wire:model.defer="photo" x-ref="photo"
+                            x-on:change="
+                                photoName = $refs.photo.files[0].name;
+                                const reader = new FileReader();
+                                reader.onload = (e) => {
+                                    photoPreview = e.target.result;
+                                };
+                                reader.readAsDataURL($refs.photo.files[0]);
+                        " />
+
+                        <!-- Current Profile Photo -->
+                        <div x-show="! photoPreview">
+                            <img class="pfp-image" src="assets/top1.png" alt="profile"
+                                class="rounded-full h-40 w-40 object-cover">
+                        </div>
+                        {{-- <!-- New Profile Photo Preview -->
+                            <div class="my-10" x-show="photoPreview" style="display: none;">
+                                <span class="block rounded-full w-40 h-40 bg-cover bg-no-repeat bg-center"
+                                    x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                                </span>
+                            </div> --}}
+
+                        {{-- Upload Button --}}
+                        <button class="pf-pp-btn" type="button" x-on:click.prevent="$refs.photo.click()">
+                            {{ __('Pilih Gambar') }}
+                        </button>
+                    </div>
+                </div>
+                <div class="pf-pass">
+                    <h3 class="pf-title">Ganti Kata Sandi</h3>
+                    <form action="" class="pf-profileform">
+                        <div class="pf-formdetail">
+                            <div class="pf-name">Kata Sandi Lama</div>
+                            <div class="pf-textfield"><input class="pf-inputpass" type="password" id="passlama"
+                                    name="nama" placeholder="Silahkan Mengisi Kata Sandi Lama" required> <img
+                                    class = "hide-pass-icon" src="assets/eye.svg" alt = "hide" id = "eyehidelama"
+                                    onclick = "hidePasswordLama()"></div>
+                        </div>
+                        <div class="pf-formdetail">
+                            <div class="pf-name">Kata Sandi Baru</div>
+                            <div class="pf-textfield"><input class="pf-inputpass" type="password" id="passbaru"
+                                    name="nama" placeholder="Silahkan Mengisi Kata Sandi Baru" required> <img
+                                    class = "hide-pass-icon" src="assets/eye.svg" alt = "hide" id = "eyehidebaru"
+                                    onclick = "hidePasswordBaru()"></div>
+                        </div>
+                        <div class="pf-submit-btn-container"><button class="pf-submit-btn">Ganti</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('components.admin.footeradmin')
+    <script>
+        function hidePasswordLama() {
+            var x = document.getElementById('passlama');
+            var eyel = document.getElementById('eyehidelama');
+
+            if (x.type === "password") {
+                x.type = "text";
+                eyel.style.opacity = 0.5;
+            } else {
+                x.type = "password";
+                eyel.style.opacity = 1;
+            }
+        }
+
+        function hidePasswordBaru() {
+            var y = document.getElementById('passbaru');
+            var eyeb = document.getElementById('eyehidebaru');
+
+            if (y.type === "password") {
+                y.type = "text";
+                eyeb.style.opacity = 0.5;
+            } else {
+                y.type = "password";
+                eyeb.style.opacity = 1;
+            }
+        }
+    </script>
+</body>
+
+</html>
---
+++ resources/views/components/shipping_service/sidebar.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/components/customer/sidebarcustomer.css')
+</head>
+
+<body>
+    <div class = "sidebar">
+        <div class = "sidebar-content">
+            <div class="sidebar-back">
+                <a href="#" class="profile"><img src="assets/side_bar/back_arrow.svg" alt="Back">
+                    <div class="profile"><img class="pp-picture" src="assets/top1.png" alt="" />
+                        <p>Ryan</p>
+                    </div>
+                </a>
+            </div>
+            <div class="sidebar-menu-container">
+                <div class="sidebar-menu">
+                    <h2>Akun</h2>
+                    <ul class = "spacing-sidebarmenu">
+                        <li><a href="#">Profil</a></li>
+                        <li><a href="#">Ganti Password</a></li>
+                    </ul>
+                </div>
+                {{-- active-page bisa dimasukkin ke class biar warna menu nya ke selected --}}
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
